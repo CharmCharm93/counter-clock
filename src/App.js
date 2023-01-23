e4780a88b6cf4c73d9522a446ab1ba9cdcc5c6f5
CharmCharm93 <caohoangmyduyen@gmail.com>
2023-01-23 12:35:35 +0700

    counter-clock-assignment

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const color = count < 0 ? "red" : count > 0 ? "green" : "black";
+  const decrease = () => setCount(count - 1);
+  const reset = () => setCount(0);
+  const increase = () => setCount(count + 1);
+
+  return (
+    <div>
+      <div className="container">
+        <h1>Counter</h1>
+        <span style={{ color: color }} id="value">
+          {count}
+        </span>
+        <div className="btn-group">
+          <button onClick={decrease} className="btn btn-decrease">
+            DECREASE
+          </button>
+          <button onClick={reset} className="btn btn-reset">
+            RESET
+          </button>
+          <button onClick={increase} className="btn btn-increase">
+            INCREASE
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
